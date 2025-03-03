edf03234b260355fc571f661b856958b4a41ef33
cahocyjei <64546089+cahocyjei@users.noreply.github.com>
2025-03-03 18:20:39 -0500

    Feature/oc/users crud (#2)
    * fix dev-dependencies[overrides:glob-version 11.0.1]
    
    * users_crud
    
    * fix users_crud[createUserDto, UserEntity]

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty()
+    name: string;
+
+    @IsString()
+    @ApiProperty()
+    lastName: string;
+
+    @IsDateString()
+    @ApiProperty()
+    birthdate: string;
+
+    @IsString()
+    @ApiProperty()
+    address: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,39 @@
-export class User {}
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    name: string;
+
+    @Column({name: 'last_name'})
+    lastName: string;
+
+    @Column()
+    birthdate: Date;
+
+    @Column()
+    address: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt?: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt?: Date;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt?: Date;
+}
