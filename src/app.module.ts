bd4ba9111236728a1240e215ddd31e55229201ec
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-03-08 08:15:21 -0500

    app module rever

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,35 +30,4 @@ import { UserEntity } from './users/entities/user.entity';
     }),
   ],
 })
-export class AppModule implements OnModuleInit {
-  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
-
-  async onModuleInit() {
-    console.log('🔄 Ejecutando seeders...');
-    await this.seedEvents();
-    console.log('✅ Seeding completado');
-  }
-  private async seedEvents() {
-    const positionRepository = this.dataSource.getRepository(PositionEntity);
-    const userRepository = this.dataSource.getRepository(UserEntity);
-
-    const positions: PositionEntity[] = [
-      { name: 'Dancer', description: 'Dancer position' },
-      { name: 'Dancer 1', description: 'Dancer position 1' },
-      { name: 'Dancer 2', description: 'Dancer position 2' },
-    ];
-    await positionRepository.insert(positions);
-    console.log('✅ Positions insertados correctamente');
-    const users: UserEntity[] = [
-      {
-        name: 'Jeiman',
-        lastName: 'Cabarcas',
-        address: 'Test',
-        birthdate: new Date(),
-        email: 'jeimancabarcas@gmail.com',
-      },
-    ];
-    await userRepository.insert(users);
-    console.log('✅ Users insertados correctamente');
-  }
-}
+export class AppModule {}
